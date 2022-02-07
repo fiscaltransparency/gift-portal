324e011aa36b4b1c827c4726a5f95ff819e9ccec
robertsonmorales <moralesso152842@gmail.com>
2022-02-07 17:37:16 +0800

    added next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    })
+
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+
+    return config
+  },
+  env: {
+    APP_GITHUB_KEY: process.env.APP_GITHUB_KEY,
+  },
+}
