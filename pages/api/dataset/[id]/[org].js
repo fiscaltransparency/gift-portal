972b34868239c067335d838b58ef36b5ae04896d
Stephen Oni <steohenoni2@gmail.com>
2022-06-03 15:25:54 +0000

    Update [org].js

--- pages/api/dataset/[id]/[org].js
+++ pages/api/dataset/[id]/[org].js
@@ -11,7 +11,7 @@ import { v4 as uuidv4 } from 'uuid'
 export default async function handler(req, res) {
   const apolloClient = initializeApollo()
   await apolloClient.query({query: PERMISSIONS})
-  const permissions = new Permissions(apolloClient.cache.extract())
+//   const permissions = new Permissions(apolloClient.cache.extract())
   res.status(200).send("In here");
 
   try {
